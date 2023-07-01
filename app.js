1bc2bdd61a20425e7e3dde9cdefa35fe05202c1a
Mak Vinícius <mvgj369@gmail.com>
2023-06-30 22:22:27 -0300

    Improving UX of entire site

--- app.js
+++ app.js
@@ -14,11 +14,21 @@ app.get("/", function(req, res) {
     res.render("home", {});
 });
 
+app.get("/professor", function(req, res) {
+    res.render("professor", {});
+});
+
 app.get("/videomaterial", function(req, res) {
     res.render("videomaterial", {});
-})
+});
 
+app.get("/informacoes", function(req, res) {
+    res.render("informacoes", {});
+});
 
+app.get("/contato", function(req, res) {
+    res.render("faleconosco", {});
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
